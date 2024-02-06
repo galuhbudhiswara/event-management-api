a6b84958554a05e5f8ce66ea58a5b05fda659d8b
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2024-02-06 14:29:03 +0700

    controlling json response

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\New_;
 
 class EventController extends Controller
 {
@@ -13,7 +15,8 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with('user')->get());
+
     }
 
     /**
@@ -33,7 +36,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -41,7 +44,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -56,7 +60,7 @@ class EventController extends Controller
             'end_time' => 'sometimes|date|after:start_time',
         ]),
     );
-    return $event;      
+    return new EventResource($event);      
 }
 
     /**
