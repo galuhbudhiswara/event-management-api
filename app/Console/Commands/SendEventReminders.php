06725e31c71ad08d59fe40dd12dd2bf3193c367e
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2024-02-11 15:12:25 +0700

    reminding about event with custom command

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends notification to all event attendees that event is starts soon';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = Event::with('attendees.user')
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
+
+        $eventCount = $events->count();
+        $eventLabel = Str::plural('event', $eventCount);
+
+        $this->info("Found {$eventCount} and {$eventLabel}");
+
+        $events->each(fn($event) => $event->attendees->each(fn($attendee) => $this->info("Notifying the user {$attendee->user_id}")));
+
+        $this->info('Command run succsessfuly');
+    }
+}
